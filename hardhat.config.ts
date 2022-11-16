5f34d94fa8cd94b4b83343ca32fc2109ec79e6cc
0x0stan <bigsml@gmail.com>
2022-11-16 14:49:36 +0800

    init nft with openzeppelin

---
+++ hardhat.config.ts
@@ -0,0 +1,8 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.5.15",
+};
+
+export default config;
